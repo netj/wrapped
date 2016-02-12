6e20693783abd6d2a0039712624c6b81821da874
Jaeho Shin <netj@cs.stanford.edu>
2016-02-12 12:22:52 -0800

    Makes sure some env vars don't mess greenplum's commands

--- greenplum.env.sh
+++ greenplum.env.sh
@@ -1,2 +1,8 @@
+# clean environment
+unset LD_LIBRARY_PATH
+unset PYTHONPATH
+unset PYTHONHOME
+# greenplum's setup
 source /dfs/scratch0/netj/wrapped/greenplum.bin/../greenplum_path.sh
+# and our own python virtualenv to isolate its dependency
 source /dfs/scratch0/netj/wrapped/greenplum.bin/../python-virtualenv/bin/activate
